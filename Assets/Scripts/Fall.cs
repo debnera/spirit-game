20e5b850a984e73235a507071af5ef646d375ace
Anton Debner <anton.debner@aalto.fi>
2018-10-22 20:49:55 +0300

    Add some random stuff to the test scene

---
+++ Assets/Scripts/Fall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    private bool triggered = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (triggered) return;
+        triggered = true;
+        var rigidBody = GetComponent<Rigidbody>();
+        if (rigidBody)
+        {
+            rigidBody.useGravity = true;
+        }
+        Destroy(this, 20f);
+    }
+}
