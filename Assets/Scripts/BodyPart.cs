cbfe22c83bc6d6c228f0f4acdfd138271ea39b0b
Anton Debner <anton.debner@aalto.fi>
2018-11-14 10:52:44 +0200

    Partial implementation

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BodyPart : MonoBehaviour
+{
+    private BodyPartRegistry registry;
+    private int ID = -1;
+    private bool attached = false;
+
+    void Start()
+    {
+        registry = BodyPartRegistry.GetInstance();
+        if (registry)
+            ID = registry.GetID(this);
+    }
+
+    public int GetID()
+    {
+        return ID;
+    }
+
+    public bool IsAttached()
+    {
+        return attached;
+    }
+
+    public void SetAttached(bool value)
+    {
+        attached = value;
+    }
+
+    public void AttachTo(GameObject parent)
+    {
+        if (!attached)
+        {
+            transform.parent = parent.transform;
+            attached = true;
+        }
+            
+    }
+}
---
+++ Assets/Scripts/BodyPartConnector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartConnector : MonoBehaviour
+{
+    public BodyPart attachedPart;
+
+    private FixedJoint joint;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    joint = GetComponent<FixedJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool IsEmpty()
+    {
+        return attachedPart != null;
+    }
+
+    public void AttachTo(BodyPart bodyPart)
+    {
+        attachedPart = bodyPart;
+        bodyPart.SetAttached(true);
+        //bodyPart.transform.parent = transform;
+        joint.connectedBody = bodyPart.gameObject.GetComponent<Rigidbody>();
+    }
+
+    public void Detach()
+    {
+        if (attachedPart)
+            attachedPart.SetAttached(false);
+        attachedPart = null;
+        joint.connectedBody = null;
+    }
+}
